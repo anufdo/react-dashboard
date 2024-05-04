e086451262a8c1cb029baf34118bb1a133a0056e
anuradha fernando <anufdo@gmail.com>
2024-05-04 13:04:57 +0530

    add tailwind css and prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-
-
 function App() {
   return (
-    <div >
-      Hello Dashboard
+    <div>
+      <p className="text-blue-500 font-bold">Hello Tailwind CSS</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
